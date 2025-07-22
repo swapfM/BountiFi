926b01c27b4902dcbf06d79eee29532f28cef765
Swapnil <singhswapnil507@gmail.com>
2025-07-22 13:23:43 +0530

    updated fronted for deployment

--- frontend/frontend/src/constants/abi.ts
+++ frontend/frontend/src/constants/abi.ts
@@ -1,4 +1,4 @@
-import bountyEscrowJson from "../../../contracts/hardhat/artifacts/contracts/Escrow.sol/BountyEscrow.json";
-import bountyFiBadgeJson from "../../../contracts/hardhat/artifacts/contracts/NFT.sol/BountiFiBadge.json";
+import bountyEscrowJson from "./BountyEscrow.json";
+import bountyFiBadgeJson from "./BountiFiBadge.json";
 export const BOUNTY_ESCROW_ABI = bountyEscrowJson.abi;
 export const BOUNTIFI_BADGE_ABI = bountyFiBadgeJson.abi;
